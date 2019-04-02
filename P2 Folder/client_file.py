32c54a246bb4ae24f710702d3c0c66efd1e6d0c3
hyaben <helena-99@hotmail.com>
2019-04-02 18:17:17 +0200

    PRACTICE 2

---
+++ P2 Folder/client_file.py
@@ -0,0 +1,113 @@
+import socket
+
+# First we are going to define our seq_class
+
+class Seq():
+
+    # Here we're going to define the instance variables with the __init__method
+
+
+    def __init__(self, strbases):
+
+        self.strbases = strbases.upper()
+
+    # Now we're going to define a regular method which gives us the length of our sequence
+
+    def len(self):
+        return len(self.strbases)
+
+    def complement(self):
+
+        # First, we're going to create a list from our sequence
+
+        new_sequence = list(self.strbases)
+
+        # Now we're going to replace the elements in the list
+
+        for number, value in enumerate(new_sequence):
+
+            if value == 'A':
+                new_sequence[number] = 'G'
+            elif value == 'G':
+                new_sequence[number] = 'A'
+            elif value == 'C':
+                new_sequence[number] = 'T'
+            elif value == 'T':
+                new_sequence[number] = 'C'
+
+        empty_string = ''
+
+        for element in new_sequence:
+            empty_string = empty_string + element
+
+        return empty_string
+
+    # Now we're going to create a method that gives us the reverse of the sequence
+
+    def reverse(self):
+        return self.strbases[::-1]
+
+    # Now we're going to create a method that gives us the number of occurences of the basis.
+
+    def count(self, base):
+
+        len_seq = len(self.strbases)
+
+        count_basis = 0
+
+        for i in range(len_seq):
+            if self.strbases[i] == base:
+                count_basis += 1
+
+        return '{} {} {}'.format(base, ':', count_basis)
+
+    # Now we're going to create a method that gives us the percentage of the basis.
+
+    def perc(self, base):
+
+        len_seq = len(self.strbases)
+
+        count_basis = 0
+
+        for i in range(len_seq):
+            if self.strbases[i] == base:
+                count_basis += 1
+
+        percentage_basis = round(float(count_basis) / len_seq * 100, 1)
+
+        return '{} {} {} {}'.format(base,':', percentage_basis, '%')
+
+
+
+# SERVER IP, PORT
+
+IP = "192.168.1.36"
+PORT = 8081
+
+# First, create the socket
+# We will always use this parameters: AF_INET y SOCK_STREAM
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# establish the connection to the Server (IP, PORT)
+
+s.connect((IP, PORT))
+
+
+while True:
+
+    # We first ask the user to enter a valid sequence
+
+    seq = input('Enter a valid sequence to proceed: ')
+
+    # We now convert the sequence into an instance
+    instance_seq = Seq(seq)
+
+    # Transformation to complement sequence
+    comp_seq = Seq(instance_seq.complement())
+
+    # Transformation to reverse sequence
+    rever_seq = Seq(comp_seq.reverse())
+
+    mssg = str.encode(rever_seq.strbases)
+
+    s.send(mssg)
---
+++ P2 Folder/server_file.py
@@ -0,0 +1,49 @@
+
+import socket
+
+# Configure the Server's IP and PORT
+PORT = 8081
+IP = "192.168.1.36"
+MAX_OPEN_REQUESTS = 5
+
+# Counting the number of connections
+number_con = 0
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    serversocket.bind((IP, PORT))
+    # become a server socket
+    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
+    serversocket.listen(MAX_OPEN_REQUESTS)
+
+    while True:
+        # accept connections from outside
+        print("Waiting for connections at {}, {} ".format(IP, PORT))
+        (clientsocket, address) = serversocket.accept()
+
+        # Another connection!e
+        number_con += 1
+
+        # Print the conection number
+        print("CONNECTION: {}. From the IP: {}".format(number_con, address))
+
+        # Read the message from the client while the connection remains
+
+        msg = clientsocket.recv(2048).decode("utf-8")
+
+        print("Message from client: {}".format(msg))
+
+        while True: # We have modified the code so it can receive several messages from a connection
+            sg = clientsocket.recv(2048).decode("utf-8")
+            print("Message from client: {}".format(sg))
+
+        clientsocket.close()
+
+except socket.error:
+    print("Problems using port {}. Do you have permission?".format(PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    serversocket.close()
